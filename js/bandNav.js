6da8706ad3c1a556d2a115d28096ea270f8e0aa2
Rick Harmelink <rimihaeg@live.nl>
2016-09-23 12:39:20 +0200

    added js to index

---
+++ js/bandNav.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Rick on 23-9-2016.
+ */
+function displayFish() {
+    document.getElementById('fish').style.display = "block"; // show Icy JJ Fish youtube div
+    document.getElementById('geert').style.display = "none"; // hide Geert Wilders youtube div
+    document.getElementById('ludwig').style.display = "none"; // hide Ludwig van Beerhoven youtube div
+    document.getElementById('kanye').style.display = "none"; // hide Kanye West youtube div
+    document.getElementById('deutchland').style.display = "none"; // hide Erlend & Steinjo youtube div
+    document.getElementById('dimebag').style.display = "none"; // hide Dimebag Darel youtube div
+    document.getElementById('corey').style.display = "none"; // hide Corey Taylor youtube div
+}
+
+function displayGeert() {
+    document.getElementById('fish').style.display = "none"; // hide Icy JJ Fish youtube div
+    document.getElementById('geert').style.display = "block"; // show Geert Wilders youtube div
+    document.getElementById('ludwig').style.display = "none"; // hide Ludwig van Beerhoven youtube div
+    document.getElementById('kanye').style.display = "none"; // hide Kanye West youtube div
+    document.getElementById('deutchland').style.display = "none"; // hide Erlend & Steinjo youtube div
+    document.getElementById('dimebag').style.display = "none"; // hide Dimebag Darel youtube div
+    document.getElementById('corey').style.display = "none"; // hide Corey Taylor youtube div
+}
+
+function displayLudwig() {
+    document.getElementById('fish').style.display = "none"; // hide Icy JJ Fish youtube div
+    document.getElementById('geert').style.display = "none"; // hide Geert Wilders youtube div
+    document.getElementById('ludwig').style.display = "block"; // show Ludwig van Beerhoven youtube div
+    document.getElementById('kanye').style.display = "none"; // hide Kanye West youtube div
+    document.getElementById('deutchland').style.display = "none"; // hide Erlend & Steinjo youtube div
+    document.getElementById('dimebag').style.display = "none"; // hide Dimebag Darel youtube div
+    document.getElementById('corey').style.display = "none"; // hide Corey Taylor youtube div
+}
+
+function displayKanye() {
+    document.getElementById('fish').style.display = "none"; // hide Icy JJ Fish youtube div
+    document.getElementById('geert').style.display = "none"; // hide Geert Wilders youtube div
+    document.getElementById('ludwig').style.display = "none"; // hide Ludwig van Beerhoven youtube div
+    document.getElementById('kanye').style.display = "block"; // show Kanye West youtube div
+    document.getElementById('deutchland').style.display = "none"; // hide Erlend & Steinjo youtube div
+    document.getElementById('dimebag').style.display = "none"; // hide Dimebag Darel youtube div
+    document.getElementById('corey').style.display = "none"; // hide Corey Taylor youtube div
+}
+
+function displayDeutchland() {
+    document.getElementById('fish').style.display = "none"; // hide Icy JJ Fish youtube div
+    document.getElementById('geert').style.display = "none"; // hide Geert Wilders youtube div
+    document.getElementById('ludwig').style.display = "none"; // hide Ludwig van Beerhoven youtube div
+    document.getElementById('kanye').style.display = "none"; // hide Kanye West youtube div
+    document.getElementById('deutchland').style.display = "block"; // show Erlend & Steinjo youtube div
+    document.getElementById('dimebag').style.display = "none"; // hide Dimebag Darel youtube div
+    document.getElementById('corey').style.display = "none"; // hide Corey Taylor youtube div
+}
+function displayDimebag() {
+    document.getElementById('fish').style.display = "none"; // hide Icy JJ Fish youtube div
+    document.getElementById('geert').style.display = "none"; // hide Geert Wilders youtube div
+    document.getElementById('ludwig').style.display = "none"; // hide Ludwig van Beerhoven youtube div
+    document.getElementById('kanye').style.display = "none"; // hide Kanye West youtube div
+    document.getElementById('deutchland').style.display = "none"; // hide Erlend & Steinjo youtube div
+    document.getElementById('dimebag').style.display = "block"; // show Dimebag Darel youtube div
+    document.getElementById('corey').style.display = "none"; // hide Corey Taylor youtube div
+}
+function displayCorey() {
+    document.getElementById('fish').style.display = "none"; // hide Icy JJ Fish youtube div
+    document.getElementById('geert').style.display = "none"; // hide Geert Wilders youtube div
+    document.getElementById('ludwig').style.display = "none"; // hide Ludwig van Beerhoven youtube div
+    document.getElementById('kanye').style.display = "none"; // hide Kanye West youtube div
+    document.getElementById('deutchland').style.display = "none"; // hide Erlend & Steinjo youtube div
+    document.getElementById('dimebag').style.display = "none"; // hide Dimebag Darel youtube div
+    document.getElementById('corey').style.display = "block"; // show Corey Taylor youtube div
+}
